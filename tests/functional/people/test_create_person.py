b3f8cd7cfc49e3f8f5394112c578e0fb4eb818d6
Stuart Wade <stewbawka@gmail.com>
2019-04-18 16:56:29 -0300

    Update test to include xfail that needs implementation

--- tests/functional/people/test_create_person.py
+++ tests/functional/people/test_create_person.py
@@ -1,25 +1,47 @@
 from http import HTTPStatus
 
+import pytest
+
 from connections.models.person import Person
 
 
-def test_can_create_person(db, testapp):
-    payload = {
+@pytest.fixture
+def person_payload():
+    return {
         'first_name': 'Bob',
         'last_name': 'Loblaw',
         'email': 'bob.loblaw@lawblog.co',
     }
 
-    res = testapp.post('/people', json=payload)
+
+def test_can_create_person(db, testapp, person_payload):
+    res = testapp.post('/people', json=person_payload)
 
     assert res.status_code == HTTPStatus.CREATED
 
-    for field in payload:
-        assert res.json[field] == payload[field]
+    for field in person_payload:
+        assert res.json[field] == person_payload[field]
     assert 'id' in res.json
 
     person = Person.query.get(res.json['id'])
 
     assert person is not None
-    for field in payload:
-        assert getattr(person, field) == payload[field]
+    for field in person_payload:
+        assert getattr(person, field) == person_payload[field]
+
+
+@pytest.mark.parametrize('field, value, error_message', [
+    pytest.param('first_name', None, 'Field may not be null.', id='missing first name'),
+    pytest.param('email', None, 'Field may not be null.', id='missing email'),
+    pytest.param('email', 'foo@bar', 'Not a valid email address.', id='invalid email',
+                 marks=pytest.mark.xfail),
+])
+def test_create_person_validations(db, testapp, person_payload, field, value, error_message):
+    person_payload[field] = value
+
+    res = testapp.post('/people', json=person_payload)
+
+    assert res.status_code == HTTPStatus.BAD_REQUEST
+    assert res.json['description'] == 'Input failed validation.'
+    errors = res.json['errors']
+    assert error_message in errors[field]
